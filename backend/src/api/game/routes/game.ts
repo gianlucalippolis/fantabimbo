41409164fa3b2088a0395b2e657915dad8f8cd25
Gianluca Lippolis <gianluca.lippolis@enhancers.it>
2025-10-14 22:32:37 +0200

    fixed games routes

--- backend/src/api/game/routes/game.ts
+++ backend/src/api/game/routes/game.ts
@@ -4,27 +4,16 @@ export default factories.createCoreRouter("api::game.game", {
   config: {
     policies: ["global::is-authenticated"],
   },
-  only: ["find", "findOne", "create", "delete"],
   routes: [
-    {
-      method: "POST",
-      path: "/games/join",
-      handler: "game.joinByCode",
-      config: {
-        auth: {
-          strategies: ["jwt"],
-        },
-      },
-    },
+    { method: "GET", path: "/games", handler: "game.find" },
+    { method: "GET", path: "/games/:id", handler: "game.findOne" },
+    { method: "POST", path: "/games", handler: "game.create" },
+    { method: "DELETE", path: "/games/:id", handler: "game.delete" },
+    { method: "POST", path: "/games/join", handler: "game.joinByCode" },
     {
       method: "POST",
       path: "/games/:id/regenerate-invite",
       handler: "game.regenerateInvite",
-      config: {
-        auth: {
-          strategies: ["jwt"],
-        },
-      },
     },
   ],
 } as any);
